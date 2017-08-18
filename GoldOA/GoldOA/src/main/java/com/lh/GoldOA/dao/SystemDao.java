7a1fa7cff1804a9439ebf02828be89b4598ac9f8
J-60-Show <rui-H-L@outlook.com>
2017-08-18 16:12:14 +0800

    在dao层增加了登录方法
    取名为login

--- GoldOA/GoldOA/src/main/java/com/lh/GoldOA/dao/SystemDao.java
+++ GoldOA/GoldOA/src/main/java/com/lh/GoldOA/dao/SystemDao.java
@@ -1,10 +1,18 @@
 package com.lh.GoldOA.dao;
 
+import com.lh.GoldOA.entity.Employee;
+
 /** 
  *主要的dao层接口
  */
 public interface SystemDao {
 
-	
+	/**
+	 * 登录方法
+	 * @param account 账号
+	 * @param password 密码
+	 * @return 返回员工对象 登录错误返回空
+	 */
+	Employee login(String account,String password);
 	
 }
