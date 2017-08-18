8e9cfd1bd6f63f9eedbc56755e8e2eff25c4f168
NashobaBrook <31085673+NashobaBrook@users.noreply.github.com>
2017-08-18 16:11:26 +0800

    在dao层增加了什么鬼我也不晓得

--- GoldOA/GoldOA/src/main/java/com/lh/GoldOA/dao/SystemDao.java
+++ GoldOA/GoldOA/src/main/java/com/lh/GoldOA/dao/SystemDao.java
@@ -1,10 +1,18 @@
 package com.lh.GoldOA.dao;
 
+import java.util.List;
+
+import com.lh.GoldOA.entity.AccountTable;
+
 /** 
  *主要的dao层接口
  */
 public interface SystemDao {
-
+	/**
+	 * 查找所有的用户
+	 * @return 返回装有Account的List集合
+	 */
+	public List<AccountTable> selectAccount();
 	
 	
 }
