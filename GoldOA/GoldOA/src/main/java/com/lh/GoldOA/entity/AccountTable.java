90d8cefe9404433b80a809fea7075903960bf1f6
NashobaBrook <31085673+NashobaBrook@users.noreply.github.com>
2017-08-18 15:50:46 +0800

    账号/商品表/员工表完成JavaBean

---
+++ GoldOA/GoldOA/src/main/java/com/lh/GoldOA/entity/AccountTable.java
@@ -0,0 +1,43 @@
+package com.lh.GoldOA.entity;
+/**
+ * 账号--持久化类
+ * @author NashobaBrook
+ * @date 2017年8月18日15:45:34
+ *
+ */
+public class AccountTable {
+	   private Integer id;//编号
+	   private String  account;//账号
+	   private String 	password;//密码
+	   private int power;//权限
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getPower() {
+		return power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public AccountTable() {
+		super();
+	}
+	   
+	   
+	   
+}
---
+++ GoldOA/GoldOA/src/main/java/com/lh/GoldOA/entity/Commodity.java
@@ -0,0 +1,44 @@
+package com.lh.GoldOA.entity;
+/**
+ * 商品表--持久化类
+ * @author NashobaBrook
+ * @date 2017年8月18日15:48:11
+ *
+ */
+public class Commodity {
+	   
+	   private Integer id;//商品编号
+	   private String comName;//商品名称
+	   private Double points;//提成点
+	   private Integer sort;//大分类 
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getComName() {
+		return comName;
+	}
+	public void setComName(String comName) {
+		this.comName = comName;
+	}
+	public Double getPoints() {
+		return points;
+	}
+	public void setPoints(Double points) {
+		this.points = points;
+	}
+	public Integer getSort() {
+		return sort;
+	}
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
+	public Commodity() {
+		super();
+	}
+	   
+	   
+	   
+}
