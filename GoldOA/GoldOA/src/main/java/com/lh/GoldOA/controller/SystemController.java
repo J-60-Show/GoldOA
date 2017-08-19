59b021475aa7be8af3c535a601c35600c05b5da9
NashobaBrook <31085673+NashobaBrook@users.noreply.github.com>
2017-08-19 23:30:23 +0800

    添加Controller,还要修改JDBC的xml
    之前JDBC的xml跟web.xml的文件名不对应.导致无法运行.
    另外建议在总的Controller中添加的Controller都是get方法发起的请求.

--- GoldOA/GoldOA/src/main/java/com/lh/GoldOA/controller/SystemController.java
+++ GoldOA/GoldOA/src/main/java/com/lh/GoldOA/controller/SystemController.java
@@ -1,12 +1,42 @@
 package com.lh.GoldOA.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 主控制器
+ * (建议只做get方法的请求)
  */
 
 @Controller
 public class SystemController {
+	/**
+	 * 主页登陆
+	 * @return
+	 */
+	@RequestMapping(value={"/index","/"},method=RequestMethod.GET)
+	public String index(){
+		return "index";
+	}
 
+	/**
+	 * function页面
+	 * @return
+	 */
+	@RequestMapping(value={"/function"},method=RequestMethod.GET)
+	public String function(){
+		return "function";
+	}
+
+	/**
+	 * staffmanagement页面
+	 * @return
+	 */
+	@RequestMapping(value={"/staffmanagement"},method=RequestMethod.GET)
+	public String staffmanagement(){
+		return "staffmanagement";
+	}
+	
+	
 }
